adc9ff527910f6dcd5abc173bba3aa6b127175d2
strahe <www@strahe.com>
2019-09-14 10:42:33 +0800

    release 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='sanic-redis',
-    version='0.1.0',
+    version='0.1.1',
     description='Adds redis support to sanic .',
     long_description='sanic-redis is a sanic framework extension which adds support for the redis.',
     url='https://github.com/strahe/sanic-redis',
